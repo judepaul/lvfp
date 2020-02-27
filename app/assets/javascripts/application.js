215ba9410de4736beab69c4c259a9b6a1bcfd2da
judesahayaraj <judepaul.arokiaraj@gmail.com>
2020-02-27 15:30:31 +0530

    Worked on accordion with plus and minus icons with font-awesome

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -64,6 +64,18 @@ $(document).ready(function () {
             speakText();
         });
 
+        // Add minus icon for collapse element which is open by default
+        $(".collapse.show").each(function(){
+        	$(this).prev(".card-header").find("i").addClass("fa-minus").removeClass("fa-plus");
+        });
+        
+        // Toggle plus minus icon on show hide of collapse element
+        $(".collapse").on('show.bs.collapse', function(){
+        	$(this).prev(".card-header").find("i").removeClass("fa-plus").addClass("fa-minus");
+        }).on('hide.bs.collapse', function(){
+        	$(this).prev(".card-header").find("i").removeClass("fa-minus").addClass("fa-plus");
+        });
+
   });
 
 
--- app/helpers/speeches_helper.rb
+++ app/helpers/speeches_helper.rb
@@ -1,2 +1,5 @@
 module SpeechesHelper
+    def articlel_count access_code_id
+        AccessCodeSpeechMap.where(access_code_id: access_code_id).count
+    end
 end
