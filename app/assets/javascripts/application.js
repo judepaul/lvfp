a53b6d9fa07c36c3fbf35b4194506aa5ed917148
judesahayaraj <judepaul.arokiaraj@gmail.com>
2020-04-10 06:41:05 +0530

    Worked on Navbar dropdown and UI issues in flip cards

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -221,6 +221,12 @@ $(document).ready(function () {
     //     $('.studio-img').show();
     //     $('.card-text').hide();
     // });
+
+    $('.navbar-light .dmenu').hover(function () {
+        $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
+    }, function () {
+        $(this).find('.sm-menu').first().stop(true, true).slideUp(105)
+    });
     
   });
 
