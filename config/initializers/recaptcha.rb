b497a6b4bf281fdd279e866324f3f164eee2f432
Jude Sahaya Paulraj A <judepaul.arokiaraj@gmail.com>
2020-10-21 18:17:03 +0530

    Revert "Worked on contact form func changes"
    This reverts commit 35b9210c5b4d7810b1a451d9773fb68c19f55812.

--- config/initializers/recaptcha.rb
+++
@@ -1,4 +0,0 @@
-Recaptcha.configure do |config|
-  config.site_key  = '6Lex2dkZAAAAANYxFew6uuLAqbVW6H1PkFfukaRW'
-  config.secret_key = '6Lex2dkZAAAAAMRnfMFEY6TkPfUAazsGHkFCeME6'
-end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20201021070102) do
 
-
   create_table "access_code_speech_maps", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "access_code_id"
     t.bigint "speech_id"
