2fde3af6916d549b96130d67ed0e8a747c60ef53
Jude Sahaya Paulraj A <judepaul.arokiaraj@gmail.com>
2020-12-03 20:43:58 +0530

    Worked on random text display in home page and footer links

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,8 @@
 module SessionsHelper
+  def random_text
+    text_arr=Array.new
+    text_arr=["Make your subscribers listen to your emails or newsletters, anytime and anywhere.",
+              "Do you send email campaigns or newsletters?<br/> Your subscribers can listen&#42; anytime and anywhere."
+            ].sample
+  end
 end
