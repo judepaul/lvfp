ead41b025c66dc76a7f74282cb581098c50ef487
Jude Sahaya Paulraj A <judepaul.arokiaraj@gmail.com>
2020-12-09 21:53:51 +0530

    Worked on changes mentioned in Login, and reset username/password pages

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,7 @@ module SessionsHelper
   def random_text
     text_arr=Array.new
     text_arr=["Make your subscribers listen to your emails or newsletters, anytime and anywhere.",
-              "Do you send email campaigns or newsletters?<br/> Your subscribers can listen&#42; anytime and anywhere."
+              "Do you send email campaigns?<br/> Your subscribers can listen&#42; to your content anytime and anywhere."
             ].sample
   end
 end
